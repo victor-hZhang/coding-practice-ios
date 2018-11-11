ba1910082237aefd36ea2dc32091cb0352b87744
Victor Zhang <victor.zhang@tattsgroup.com>
2018-11-11 11:18:41 +1000

    created project

---
+++ Fun App/Fun App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun App
+//
+//  Created by Victor Zhang on 11/11/18.
+//  Copyright © 2018 Victor Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
